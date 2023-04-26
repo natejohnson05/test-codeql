5fee62e35a3a1f9921e598151766b3282d7b23f6
Nate Johnson <81366847+natejohnson05@users.noreply.github.com>
2023-04-26 18:50:27 -0400

    Update example.js

--- example.js
+++ example.js
@@ -1,9 +1,4 @@
-// Option 1: As a statically declared enviroment variable
-const process = require('node:process');
-
-process.env.NODE_OPTIONS = '--insecure-http-parser';
-
-// Option 2: Statically declared at server/request creation
+// Option 1: Statically declared at server/request creation
 const http = require('node:http');
 
 http.createServer({
@@ -11,6 +6,11 @@ http.createServer({
 }, (req, res) => {
     res.end('hello world\n');
 });
+
+// Option 2: As a statically declared enviroment variable
+const process = require('node:process');
+
+process.env.NODE_OPTIONS = '--insecure-http-parser';
 
 // Option 3: As a cli parameter
 // This is dynamic and thus uncoverable
